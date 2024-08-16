6e0d598503d97436b13e092a8be0895bb8ba4626
Marko Arsić <marko@hypetech.io>
2024-08-16 21:28:05 +0200

    Update gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.5.1")
+        classpath("com.android.tools.build:gradle:8.5.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
         classpath("io.realm:realm-gradle-plugin:10.15.1")
 
